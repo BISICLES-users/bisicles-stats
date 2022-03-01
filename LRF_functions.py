5bbff9893dce8d9e43028be2db05693449e4b934
“Claire <claire.donnelly@knmi.nl>
2022-03-01 16:37:57 +0000

    Corrected a mistake in the LRF calculation

--- LRF_functions.py
+++ LRF_functions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def LRF_calc(df):
+def LRF_calc(df,bm):
     '''Function to calculate a Linear Response Function from 
     Levermann et al. (2020) based on stats output from BISICLES
     df: dataframe containing BISICLES data
@@ -10,16 +10,23 @@ def LRF_calc(df):
     
     sea_area = 361.8
     rho_i = 0.917
-
+    
+    #df['vafdiff'] = df.volumeAbove - df.volumeAbove.iloc[0]
     df['ice_mass'] = (df.volumeAbove/(10**9)) * rho_i
     df['SLE'] = df.ice_mass*(1/sea_area)
     df['SLEm'] = -(df.SLE)/1000
     df['difference'] = df.SLEm.diff()
-    df['LRF'] = df.difference/4.0
+    #df['difforig'] = df.difference - df.difference.iloc[1]
+    #df['LRF'] = df.difforig/bm
+    df['LRF'] = df.difference/bm
     df['SMA10'] = df.LRF.rolling(10).mean()
     return df
 
 def LRF_ts(df,key,plot_path):
+    '''Function to plot LRF from Levermann et al. (2020) based on
+    dataframe created in LRF_calc. Returns a plot with LRF and
+    10 year running mean'''
+    
     plot = plt.plot(df['time'],df['LRF'])
     plt.plot(df['time'],df['SMA10'])
     plt.xlabel("Time (years)")
